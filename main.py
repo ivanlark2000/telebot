37aa7117a6ab38b6b4f178a2bc2b5f7eb8d1b9d6
ivan <ivaxen2000@gmail.com>
2022-03-30 18:05:54 +0200

    start

---
+++ main.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.utils import executor
+
+from config import TOKEN
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+button_hi = KeyboardButton('Привет! 👋')
+button_fact = KeyboardButton('Факт')
+greet_kb1 = ReplyKeyboardMarkup(resize_keyboard=True).add(button_hi)
+greet_kb2 = ReplyKeyboardMarkup(
+    resize_keyboard=True, one_time_keyboard=True
+).add(button_hi)
+button1 = KeyboardButton('1️⃣')
+button2 = KeyboardButton('2️⃣')
+button3 = KeyboardButton('3️⃣')
+
+markup3 = ReplyKeyboardMarkup().add(
+    button1).add(button2).add(button3)
+
+markup4 = ReplyKeyboardMarkup().row(
+    button1, button2, button3
+)
+
+markup5 = ReplyKeyboardMarkup().row(
+    button1, button2, button3
+).add(KeyboardButton('Средний ряд'))
+
+button4 = KeyboardButton('4️⃣')
+button5 = KeyboardButton('5️⃣')
+button6 = KeyboardButton('6️⃣')
+markup5.row(button4, button5)
+markup5.insert(button6)
+
+# keyboards.py
+markup_request = ReplyKeyboardMarkup(resize_keyboard=True).add(
+    KeyboardButton('Отправить свой контакт ☎️', request_contact=True)
+).add(
+    KeyboardButton('Отправить свою локацию 🗺️', request_location=True)
+)
+
+# keyboards.py
+markup_big = ReplyKeyboardMarkup()
+
+markup_big.add(
+    button1, button2, button3, button4, button5, button6
+)
+markup_big.row(
+    button1, button2, button3, button4, button5, button6
+)
+
+markup_big.row(button4, button2)
+markup_big.add(button3, button2)
+markup_big.insert(button1)
+markup_big.insert(button6)
+markup_big.insert(KeyboardButton('9️⃣'))
+
+
+# bot.py
+@dp.message_handler(commands=['hi7'])
+async def process_hi7_command(message: types.Message):
+    await message.reply("Седьмое - все методы вместе", reply_markup=markup_big)
+
+
+# bot.py
+@dp.message_handler(commands=['hi6'])
+async def process_hi6_command(message: types.Message):
+    await message.reply("Шестое - запрашиваем контакт и геолокацию \nЭти две кнопки не зависят друг от друга",
+                        reply_markup=markup_request)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!", reply_markup=greet_kb1)
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отправлю этот текст тебе в ответ!")
+
+
+@dp.message_handler(commands=['hi4'])
+async def process_hi4_command(message: types.Message):
+    await message.reply("Четвертое - расставляем кнопки в ряд", reply_markup=markup4)
+
+
+@dp.message_handler(commands=['hi5'])
+async def process_hi5_command(message: types.Message):
+    await message.reply("Пятое - добавляем ряды кнопок", reply_markup=markup5)
+
+
+# @dp.message_handler()
+# async def echo_message(msg: types.Message):
+#     await bot.send_message(msg.from_user.id, msg.text)
+
+@dp.message_handler(commands=['hi3'])
+async def process_hi3_command(message: types.Message):
+    await message.reply("Третье - добавляем больше кнопок", reply_markup=markup3)
+
+
+# bot.py
+@dp.message_handler(commands=['hi2'])
+async def process_hi2_command(message: types.Message):
+    await message.reply("Второе - прячем клавиатуру после одного нажатия", reply_markup=greet_kb2)
+
+
+@dp.message_handler(commands=['rm'])
+async def process_rm_command(message: types.Message):
+    await message.reply("Убираем шаблоны сообщений", reply_markup=ReplyKeyboardRemove())
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
